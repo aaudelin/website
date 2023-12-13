4b378d0645ac8505cde189897966181f4e48865e
Aurélien Audelin <aurelien.audelin@gmail.com>
2023-12-13 13:55:53 +0100

    refactor: update analytics with partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 import vercel from "@astrojs/vercel/serverless";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [
+      partytown({
+        config: {
+          forward: ["dataLayer.push"],
+        },
+      })
+  ]
 });
